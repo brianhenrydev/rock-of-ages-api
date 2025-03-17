9d0bfbf1d0d5b0cd615663a44f3ba11dffc6c4c0
Brian Henry <brianhenrytech@gmail.com>
2025-03-17 15:16:41 -0500

    Added fixtures for database seeding and implemented expanded rocks

--- rockapi/views/rock.py
+++ rockapi/views/rock.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.models import User
 from django.http import HttpResponseServerError
 from rest_framework import serializers, status
 from rest_framework.views import Response
 from rest_framework.viewsets import ViewSet
 from rockapi.models import Rock
+from rockapi.models import Type
 
 
 class RockView(ViewSet):
@@ -20,11 +22,29 @@ class RockView(ViewSet):
             return HttpResponseServerError(ex)
 
 
+class RockTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Type
+        fields = ("label",)
+
+
+class RockUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name")
+
+
 class RockSerializer(serializers.ModelSerializer):
+    type = RockTypeSerializer(many=False)
+
+    user = RockUserSerializer(many=False)
+
     class Meta:
         model = Rock
         fields = (
             "id",
             "name",
             "weight",
+            "user",
+            "type",
         )
